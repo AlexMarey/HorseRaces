889245686e6c7513faba972cd04146dbdc2ecbdd
AlexMarey <alx.9.mar@gmail.com>
2024-08-04 22:50:44 -0400

    Using a collision shape 2d to recognize a mouse click and flip a card

--- scenes/game/Card.gd
+++ scenes/game/Card.gd
@@ -2,8 +2,6 @@ extends Node2D
 
 signal flip
 
-var suit="clubs"
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -13,8 +11,9 @@ func _process(delta):
 	pass
 
 func flip_card():
+	position.x += position.x + 250
 	$AnimatedSprite2D.play("clubs2")
-	flip.emit(suit)
 
-func _on_timer_timeout():
-	flip_card()
+func _on_area_2d_input_event(viewport, event, shape_idx):
+	if(event.is_action_pressed("mouse_left_click")):
+		flip_card()
