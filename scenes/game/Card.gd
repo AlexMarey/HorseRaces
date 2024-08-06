1550f5e5932ae4196121879c46e7b83561946519
Alex Marey <alx.9.mar@gmail.com>
2024-08-06 19:38:38 -0400

    added all card animations and simple random flipper

--- scenes/game/Card.gd
+++ scenes/game/Card.gd
@@ -2,6 +2,9 @@ extends Node2D
 
 signal flip
 
+var suits: Array = ['clubs', 'diamond','hearts','spades']
+var cardValues: Array = ['2','3','4','5','6','7','8','9','10','Jack','Queen','King','Ace']
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -12,7 +15,11 @@ func _process(delta):
 
 func flip_card():
 	position.x += position.x + 250
-	$AnimatedSprite2D.play("clubs2")
+	var suitIndex = randi_range(0,3)
+	var cardIndex = randi_range(0,12)
+	var animationName = suits[suitIndex]+cardValues[cardIndex]
+	print(animationName)
+	$AnimatedSprite2D.play(animationName)
 
 func _on_area_2d_input_event(viewport, event, shape_idx):
 	if(event.is_action_pressed("mouse_left_click")):
