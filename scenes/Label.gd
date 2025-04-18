c95d6ab0d59b388c48dfd0b44e915add2ad367e6
AlexMarey <alx.9.mar@gmail.com>
2025-04-18 08:58:54 -0400

    Changed end label to capitilize, laid out plan in comments

--- scenes/Label.gd
+++ scenes/Label.gd
@@ -2,6 +2,6 @@ extends Label
 
 
 func _on_finish_line_race_finished(raceWinner: String):
-	self.text = raceWinner + " Wins!!!"
+	self.text = raceWinner.capitalize() + " Wins!!!"
 	self.visible = true
 
--- scenes/Main.gd
+++ scenes/Main.gd
@@ -1,5 +1,18 @@
 extends Node
 
+# To Do: 
+# Game over when finished line reached 
+#   Canvas Layer: Label, Restart Button, Quit Button
+#     - Make sure deck can't be clicked anymore
+#     - Reset Game: Go back to game begining
+# Name Hoses - Start screen (UI), names appear above heads
+# Polish Game
+# Done
+# Extra Ideas
+# Automatic mode - no deck clicks just go
+# Betting - Create Odds based on deck/penalty deck?
+# Campaign mode - Own a horse, influence the deck, bet on cards
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
